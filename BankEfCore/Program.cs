4f5629ea0561ed442de843ea46eb8d43cfdfd99f
GwenBleidd <thegwyenbleidd@gmail.com>
2021-06-24 01:57:52 +0600

    Add project files.

---
+++ BankEfCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankEfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
